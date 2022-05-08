4de3551a30a778d64cc3a45a8832602a31848f70
ndrslmpk <andreas.lempik@gmail.com>
2022-05-08 13:32:26 +0200

    Config | suppress unnecessary deprecation  message

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -6,7 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+# Turn off FSADeprecationWarning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # Connect to the database
-
-
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost:5432/fyyur'
